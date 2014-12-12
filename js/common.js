07a51c5489ab457f8ff9c847335615685f39fd8c
Valera19900 <kaktuz34@gmail.com>
2014-12-12 21:22:25 +0200

    pages: blog, inner_blog

---
+++ js/common.js
@@ -0,0 +1,11 @@
+head.ready(function() {
+
+	// $(document).on("click", function(){
+			
+	// });
+	$('.js-open-nav').on('click', function(){
+		$(this).toggleClass('is-active');
+		$('.js-nav').toggleClass('is-active');
+	});
+
+});
