9386074af491e0e4fc1a84a04d2179147fb8467a
Valera19900 <kaktuz34@gmail.com>
2014-12-14 17:31:07 +0200

    upd

--- js/common.js
+++ js/common.js
@@ -8,5 +8,25 @@ head.ready(function() {
 		$('.js-nav').toggleClass('is-active');
 		$('body').toggleClass('is-active');
 	});
+	
+	//add to top button
+	$(document).scroll(function() {    
+		var scroll = $(this).scrollTop();
+		if (scroll >= 400) {
+			$(".scrolltop").addClass("is-active");
+		}
+		else{
+			$(".scrolltop").removeClass("is-active");
+		}
+	});
+
+	// scrollTop
+	 $(".scrolltop").on('click', function(){
+	  var page = $(this).attr("href");
+	  $('html, body').animate({
+	   scrollTop: $(page).offset().top + 20
+	  }, 600);
+	  return false;
+	 });
 
 });
