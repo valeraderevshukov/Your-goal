54f765c16dcef05c0f9f202c51a2c5da9c472d0f
Valera19900 <kaktuz34@gmail.com>
2014-12-16 10:55:00 +0200

    fixed scrollTopm

--- js/common.js
+++ js/common.js
@@ -9,34 +9,26 @@ head.ready(function() {
 		$('body').toggleClass('is-active');
 	});
 	
-	// add to top button
+	// scrollTop
 	$(document).scroll(function() {    
 		var scroll = $(this).scrollTop();
-		if (scroll >= 400) {
-			$(".scrolltop").addClass("is-active");
+		var h_doc = $(document).height();
+		var h_wind = $('.height').height();
+		var h_footer = $('.footer').height();
+		var scroll_position = h_doc - h_wind - h_footer;
+		// alert(scroll_position)
+		if (scroll > 0) {
+			$(".scrolltop").fadeIn();
+			if (scroll >= scroll_position) {
+				$(".scrolltop").css({'position':'absolute'});
+			}else{
+				$(".scrolltop").css({'position':'fixed'});
+			}
 		}
 		else{
-			$(".scrolltop").removeClass("is-active");
+			$(".scrolltop").fadeOut();
 		}
 	});
-
-	$(function () {
-	    var a = $(".scrolltop"),
-	        c = a.offset(),
-	        d = a.outerHeight(!0),
-	        e = $(".footer").offset();
-	    $(document).scroll(function () {
-	        var b = $(this).scrollTop(),
-	            b = e.top - (b + d + c.top);
-	        0 < b ? a.css({
-	            top: c.top
-	        }) : a.css({
-	            top: c.top + b
-	        })
-	    })
-	});
-	
-	// scrollTop
 	$(".scrolltop").on('click', function(){
 		var page = $(this).attr("href");
 		$('html, body').animate({
@@ -44,4 +36,20 @@ head.ready(function() {
 		}, 600);
 		return false;
 	});
+	// $(function () {
+	// 	var a = $(".scrolltop"),
+	// 		c = a.offset(),
+	// 		d = a.outerHeight(!0),
+	// 		e = $(".footer").offset();
+	// 	$(document).scroll(function () {
+	// 		var b = $(this).scrollTop(),
+	// 			b = e.top - (b + d + c.top);
+	// 		0 < b ? a.css({
+	// 			top: c.top
+	// 		}) : a.css({
+	// 			top: c.top + b
+	// 		})
+	// 	})
+	// });
+	
 });
